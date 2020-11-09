7c7730dbbc1a545786dbb3c4c0942e144ab20369
Corbin Crutchley <crutchcorn@gmail.com>
2020-11-08 22:04:31 -0800

    Initial work to download article HTML and pictures

---
+++ get_article.py
@@ -0,0 +1,73 @@
+import os
+from errno import EEXIST
+from os import makedirs
+from urllib.request import urlopen
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.remote.webelement import WebElement
+import re
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(1)
+
+def findImgSrc(imgEl: WebElement):
+    srcset = imgEl.get_attribute('srcset')
+    if not srcset:
+        return ['', '']
+    img_ids = re.findall(r'^.*\/(.*?)\s80w', srcset)
+    return [f'https://i.kinja-img.com/gawker-media/image/upload/{img_ids[0]}', img_ids[0]]
+
+def make_sure_path_exists(path):
+    try:
+        makedirs(path)
+    except OSError as exception:
+        if exception.errno != EEXIST:
+            raise
+
+
+dirname = os.path.join(os.path.dirname(__file__), 'dist')
+
+
+def downloadPage(url: str):
+    # Given a URL: https://rockmandash12.kinja.com/rockmandash-rambles-an-explanation-on-my-review-system-1619265485
+    # Stub = rockmandash-rambles-an-explanation-on-my-review-system-1619265485
+    stub_regex = re.findall(r'^.*\/(.*?)$', url)
+
+    stub = stub_regex[0]
+
+    stub_folder_path = os.path.join(dirname, stub)
+    make_sure_path_exists(stub_folder_path)
+
+    post_body = driver.find_element_by_css_selector('.js_post-content')
+
+    print('stub', stub)
+
+    # Image downloading
+    imgs = post_body.find_elements_by_css_selector('img')
+    for img in imgs:
+        img_src, img_id = findImgSrc(img)
+        if not img_src:
+            continue
+        print(img_src, img_id)
+        img_path = os.path.join(stub_folder_path, img_id)
+        f = open(img_path, 'wb')
+        f.write(urlopen(img_src).read())
+        f.close()
+
+    # Post HTML
+    inner_html: str = post_body.get_property('innerHTML')
+    with open(os.path.join(stub_folder_path, 'index.html'), "w", encoding="utf-8") as text_file:
+        text_file.write(inner_html)
+
+    # Metadata
+
+filepath = os.path.join(os.path.dirname(__file__), 'article_list.txt')
+
+list_file = open(filepath, "r").read().split('\n')
+
+for line in list_file:
+    driver.get(line)
+    downloadPage(line)
+
+# close the browser window
+driver.quit()
